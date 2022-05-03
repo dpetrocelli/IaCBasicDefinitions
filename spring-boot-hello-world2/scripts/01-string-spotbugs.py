a325b1e06e794d2b49b7de3ac85934b3bbda3e7c
David Petrocelli <david.petrocelli@cloudhesive.com>
2022-05-03 13:36:48 -0300

    runners ready

---
+++ spring-boot-hello-world2/scripts/01-string-spotbugs.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import shutil
+
+sys.path.append(".")
+from ScriptBuilder import ScriptBuilder
+
+# [STEP 1] - Instantiate the builder class
+try:
+    string = sys.argv[1]
+    try: 
+        sb = ScriptBuilder()
+        # SPOT -> BUGINSTANCE
+        checkr="BugInstance size is"
+        grep=sb.grep(string.splitlines(),checkr, True)
+
+        # REGEX 
+        # -> convert to int
+        # -> String to be analyzed (grep result)
+        # -> String pattern (ALWAYS WITH '' single quotes)
+        regex = "'{}'.split('{}')[1].strip()".format(grep,checkr)
+        result= sb.check_result(regex)
+        
+        sb.printer("0", checkr, result)
+    except Exception as err: 
+        print (err)
+        exit (1) 
+
+    try:  
+        # SPOT -> ErrorSizeIS
+        checkr="Error size is"
+        grep=sb.grep(string.splitlines(),checkr, True)
+        regex = "'{}'.split('{}')[1].strip()".format(grep, checkr)
+        result= sb.check_result(regex)
+        sb.printer("0",checkr,result)
+    except Exception as err: 
+        print (err)
+        exit (1) 
+
+except:
+    print ("variables not passed")
+
+
+
