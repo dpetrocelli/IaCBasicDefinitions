9b63153c03accd9676e1f6c495b32afe1267eda8
David Petrocelli <david.petrocelli@cloudhesive.com>
2022-05-04 11:31:46 -0300

    nico run

--- spring-boot-hello-world2/scripts/02-mvntest.py
+++ spring-boot-hello-world2/scripts/02-mvntest.py
@@ -20,7 +20,7 @@ command = "test"
 mvnTest = "cd {} ; {} {}".format(directory, binary, command)
 testResult=sb.runner(mvnTest)
 
-
+# Ask for Failures:
 try:
     checkr="Failures:"
     grep=sb.grep(testResult[1].splitlines(),checkr, True)
@@ -36,33 +36,33 @@ except Exception as err:
 
 # #Ask for Errors:
 
-# try:
-#     checkr="Errors:"
-#     grep=sb.grep(testResult[1].splitlines(),checkr, True)
-#     regex = "'{}'.split('{}')[1].split(',')[0].strip()".format(grep, checkr)
+try:
+    checkr="Errors:"
+    grep=sb.grep(testResult[1].splitlines(),checkr, True)
+    regex = "'{}'.split('{}')[1].split(',')[0].strip()".format(grep, checkr)
     
-#     result = sb.check_result(regex)
-#     sb.printer("0", checkr, result)
-# except Exception as err: 
-#     print (err)
-#     exit (1)
+    result = sb.check_result(regex)
+    sb.printer("0", checkr, result)
+except Exception as err: 
+    print (err)
+    exit (1)
     
     
-# # # Ask for Skipped:
+# Ask for Skipped:
 
-# try:
-#     checkr="Skipped:"
-#     grep=sb.grep(testResult[1].splitlines(),checkr, True)
+try:
+    checkr="Skipped:"
+    grep=sb.grep(testResult[1].splitlines(),checkr, True)
     
-#     regex = "('{}'.split('{}')[1].strip())".format(grep, checkr)
+    regex = "('{}'.split('{}')[1].strip())".format(grep, checkr)
     
-#     result = sb.check_result(regex)
-#     sb.printer("0", checkr, result)
-#     # ansi_escape = re.compile(r'\x1bm')
+    result = sb.check_result(regex)
+    sb.printer("0", checkr, result)
+    # ansi_escape = re.compile(r'\x1bm')
     
-#     # ansi_escape.sub('', result)
-#     # print (int(result))
-#     #sb.printer(result, checkr, result)
-# except Exception as err: 
-#     print (err)
-#     exit (1)
+    # ansi_escape.sub('', result)
+    # print (int(result))
+    #sb.printer(result, checkr, result)
+except Exception as err: 
+    print (err)
+    exit (1)
